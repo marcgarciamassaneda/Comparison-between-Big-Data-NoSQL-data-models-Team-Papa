7c66ad61ac5594f1a64fde089bc01c4d17ddeec2
Marc Garcia <marcgarciamassaneda@gmail.com>
2022-12-05 14:36:42 -0500

    -y added and commented part.

--- benchmark_script.sh
+++ benchmark_script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update
 sudo apt install default-jre -y
 sudo apt install default-jdk -y
@@ -5,11 +6,11 @@ sudo apt update
 sudo apt install maven -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
-sudo usermod -aG docker ${USER}
-su - ${USER}
+#sudo usermod -aG docker ${USER}
+#su - ${USER}
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
