f48fe9d2d9e5080bec1967c10775f8499d8d0af5
Marc Garcia <marcgarciamassaneda@gmail.com>
2022-12-05 13:51:28 -0500

    first version of the benchmark bash file.

---
+++ benchmark_cassandra_script.sh
@@ -0,0 +1,25 @@
+sudo apt update
+sudo apt install default-jre -y
+sudo apt install default-jdk -y
+sudo apt update
+sudo apt install maven -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+su - ${USER}
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
+tar xfvz ycsb-0.17.0.tar.gz
+cd ycsb-0.17.0
+mkdir database
+cd database
+sudo apt install python2 -y
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
+docker-compose -f docker-compose-redis.yml up
+cd ..
+./bin/ycsb load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > outputLoad.txt
+./bin/ycsb run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > outputRun.txt
