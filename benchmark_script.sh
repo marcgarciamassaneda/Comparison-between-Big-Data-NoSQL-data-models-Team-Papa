0d021e5351fc0d285eced86f433a465aff0889be
Marc Garcia <marcgarciamassaneda@gmail.com>
2022-12-05 14:33:25 -0500

    -y added.

--- benchmark_script.sh
+++ benchmark_script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update
 sudo apt install default-jre -y
 sudo apt install default-jdk -y
@@ -5,7 +6,7 @@ sudo apt update
 sudo apt install maven -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
